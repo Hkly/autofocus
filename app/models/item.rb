41f346506e2d7db248246df963adff543501020c
hkly <hannah.k.yiu@gmail.com>
2019-08-08 14:02:16 -0700

    Only show unfocused items in the regular items list

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   scope :completed, -> { where(completed: true) }
   scope :incomplete, -> { where(completed: false) }
   scope :ordered_by_date_completed, -> { order(date_completed: :desc) }
+  scope :unfocused, -> { left_outer_joins(:focus).where("focuses.id": nil) }
 
   after_update :unfocus, if: :focused_completed
 
