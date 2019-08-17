168812cc580cf541317b79953bbb4c6a242c31ba
hkly <hannah.k.yiu@gmail.com>
2019-08-16 17:10:57 -0700

    Some base styling

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,8 @@
+class ListsController < ApplicationController
+  before_action :ensure_logged_in
+
+  def index
+
+  end
+
+end
--- app/helpers/list_helper.rb
+++ app/helpers/list_helper.rb
@@ -2,18 +2,18 @@ module ListHelper
   def link_to_complete item
       params = { "item[completed]": true }
 
-    link_to icon("fas", "check"), item_path(item, params), method: :patch, title: "Complete"
+    link_to icon("fas", "check"), item_path(item, params), method: :patch, title: "Complete", alt: "Complete"
   end
 
   def link_to_delete item
-     link_to icon("fas", "times"), item_path(item), method: :delete, title: "Delete"
+     link_to icon("fas", "times"), item_path(item), method: :delete, title: "Delete", alt: "Delete"
   end
 
   def link_to_focus item
-    link_to icon("fas", "star"), focus_item_path(item), method: :post, title: "Focus"
+    link_to icon("fas", "star"), focus_item_path(item), method: :post, title: "Focus", alt: "Focus"
   end
 
   def link_to_unfocus item
-    link_to icon("far", "star"), unfocus_item_path(item), method: :delete, title: "Unfocus"
+    link_to icon("far", "star"), unfocus_item_path(item), method: :delete, title: "Unfocus", alt: "Unfocus"
   end
 end
